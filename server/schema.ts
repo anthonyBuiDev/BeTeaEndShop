b3357e0f4cad4179218bb42b54118209b2018679
anthonybuidev <thanhnambui201@gmail.com>
2024-05-31 09:42:38 +0700

    add email token

--- server/schema.ts
+++ server/schema.ts
@@ -46,3 +46,17 @@ export const accounts = pgTable(
     }),
   })
 )
+
+export const emailTokens = pgTable(
+  "email_token",
+  {
+    id: text("id").notNull(),
+    token: text("token").notNull(),
+    expires: timestamp("expires", { mode: "date" }).notNull(),
+  },
+  (et) => ({
+    compositePk: primaryKey({
+      columns: [et.id, et.token],
+    }),
+  })
+)
