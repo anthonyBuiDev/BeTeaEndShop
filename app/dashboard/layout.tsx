71f1f20756e1e18327a8579d44e4a7d43e9847cf
anthonybuidev <thanhnambui201@gmail.com>
2024-06-05 14:46:52 +0700

    layout dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,54 @@
-export default function DashboardLayout({
+import DashboardNav from "@/components/navigation/dashboard-nav";
+import { auth } from "@/server/auth";
+import { BarChart, Package, PenSquare, Settings, Truck } from "lucide-react";
+
+export default async function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className="">{children}</div>;
+  const session = await auth();
+
+  const userLinks = [
+    {
+      label: "Orders",
+      path: "/dashboard/orders",
+      icon: <Truck size={16} />,
+    },
+    {
+      label: "Settings",
+      path: "/dashboard/settings",
+      icon: <Settings size={16} />,
+    },
+  ] as const;
+
+  const adminLinks =
+    session?.user.role === "admin"
+      ? [
+          {
+            label: "Analytics",
+            path: "/dashboard/analytics",
+            icon: <BarChart size={16} />,
+          },
+          {
+            label: "Create",
+            path: "/dashboard/add-product",
+            icon: <PenSquare size={16} />,
+          },
+          {
+            label: "Products",
+            path: "/dashboard/products",
+            icon: <Package size={16} />,
+          },
+        ]
+      : [];
+
+  const allLinks = [...adminLinks, ...userLinks];
+
+  return (
+    <div className="">
+      <DashboardNav allLinks={allLinks} />
+      {children}
+    </div>
+  );
 }
