b983bd47bd98b0781cbdf479b7cb632c17986458
anthonybuidev <thanhnambui201@gmail.com>
2024-06-08 15:37:15 +0700

    show product

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
+import Products from "@/components/products/page";
+import { db } from "@/server";
+
+export default async function Home() {
+  const data = await db.query.productVariants.findMany({
+    with: {
+      variantImages: true,
+      variantTags: true,
+      product: true,
+    },
+    orderBy: (productVariants, { desc }) => [desc(productVariants.id)],
+  });
   return (
     <main>
-      <h1>Welcome to NextJs</h1>
+      <Products variants={data} />
     </main>
   );
 }
---
+++ components/products/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import formatPrice from "@/lib/format-price";
+import { VariantsWithProduct } from "@/lib/infer-type";
+import Image from "next/image";
+import Link from "next/link";
+import { Badge } from "../ui/badge";
+
+type ProductTypes = {
+  variants: VariantsWithProduct[];
+};
+
+export default function Products({ variants }: ProductTypes) {
+  return (
+    <main className="grid gap-12 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      {variants.map((variant) => (
+        <Link
+          className="py-2"
+          key={variant.id}
+          href={`/products/${variant.id}?id=${variant.id}&productID=${variant.productID}&price=${variant.product.price}&title=${variant.product.title}&type=${variant.productType}&image=${variant.variantImages[0].url}`}
+        >
+          <Image
+            className="rounded-md pb-2"
+            src={variant.variantImages[0].url}
+            width={720}
+            height={480}
+            alt={variant.product.title}
+            loading="lazy"
+          />
+          <div className="flex justify-between">
+            <div className="font-medium">
+              <h2>{variant.product.title}</h2>
+              <p className="text-sm text-muted-foreground">
+                {variant.productType}
+              </p>
+            </div>
+            <div>
+              <Badge className="text-sm" variant={"secondary"}>
+                {formatPrice(variant.product.price)}
+              </Badge>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </main>
+  );
+}
